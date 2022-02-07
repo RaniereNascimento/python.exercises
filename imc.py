a1b4fb5fa3bdd596dfcefa7b971e4018934060fc
Raniere Nascimento <91237799+RanoArts@users.noreply.github.com>
2022-02-07 10:05:25 -0300

    Create imc.py

---
+++ imc.py
@@ -0,0 +1,8 @@
+#Programa que calcula o índice de massa corporal
+#Inserirdo dados no programa
+altura = float(input("insira sua altura: "))
+peso = float(input("insira seu peso: "))
+#Processamento de dados
+imc = peso/altura**2
+# Retornando o valor processado e imprimindo o resultado na tela
+print("O valor de seu imc é: ", imc,".")
